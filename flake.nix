184112987c864cefa34e00b5fa3eb6c918f5e111
cnnd <hi@cnnd.dev>
2024-06-22 16:15:06 +0100

    restructure and spicetify

--- flake.nix
+++ flake.nix
@@ -4,8 +4,11 @@
   inputs = {
     # might as well use the 24.05 version
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # add spicetify
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
@@ -13,8 +16,9 @@
     nixosConfigurations.komodo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./nixos/configuration.nix
 
+        ./modules/spicetify.nix
         # add home-manager module
         # this makes home-manager deployment automatic
         home-manager.nixosModules.home-manager
@@ -23,8 +27,9 @@
           home-manager.useUserPackages = true;
 
           # yet again, replace with your own info (unless you're also called cnnd... but that would be weird)
-          home-manager.users.cnnd = import ./home.nix;
+          home-manager.users.cnnd = import ./home-manager/home.nix;
           # here we can use home-manager.extraSpecialArgs to pass arguments to home.nix, for example with spicetify
+          home-manager.extraSpecialArgs = {inherit inputs;};
         }
       ];
     };
